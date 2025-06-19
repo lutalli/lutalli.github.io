f2312d0da107f2c75d5547b984aa74e61b551f08
Lutalli <lutallium@gmail.com>
2025-06-19 13:15:27 +0200

    Update color scheme

--- js/polymerase.js
+++ js/polymerase.js
@@ -1,28 +1,9 @@
-const bookmarkedWebsiteColors = {
-    "epsilon": "#6cf",
-    "iota": "#9f9",
-    "beta": "#6ff",
-    "pi": "#f9f",
-    "rho": "#cf9",
-    "sigma": "#c9f",
-    "tau": "#9fc",
-    "mu": "#9cf",
-    "psi": "#f9c"
-};
-
-const AMLVColors = {
-    "anime": "#ff99c0",
-    "light_novels": "#4ffca6",
-    "manga": "#66c2ff",
-    "visual_novels": "#59f3eb"
-};
-
 class synthesize {
     static bookmarkedWebsites(category) {
 	fetch(`/db/bookmarked-websites/${category}.json`)
 	    .then((resp) => resp.json())
 	    .then((json) => json.forEach((site) => {
-		$("#ul-bookmarked-websites").append(`<li><a href="${site["link"]}" style="color: ${bookmarkedWebsiteColors[category]};">${site["title"]}</a></li>\n`);
+		$("#ul-bookmarked-websites").append(`<li><a href="${site["link"]}">${site["title"]}</a></li>\n`);
 	    }));
     }
 
@@ -34,7 +15,7 @@ class synthesize {
 		if (log["title-en"] != ".") {
 		    title += `<br /><b>${log["title-en"]}</b>`;
 		}
-		$("#ul-amlv-logs").append(`<li><span style="color: ${AMLVColors[category]};">${title}</span><br />${log["detail-en"]}</li>\n`);
+		$("#ul-amlv-logs").append(`<li><span style="color: var(--col-s-2);">${title}</span><br />${log["detail-en"]}</li>\n`);
 	    }));
     }
 }
