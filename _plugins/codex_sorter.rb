bbfa60198cefe96867d0892c77c7ee8beb851747
Lutalli <47910890+lutalli@users.noreply.github.com>
2026-03-22 01:27:15 +0800

    codex_sorter: Exclude "intro"

--- _plugins/codex_sorter.rb
+++ _plugins/codex_sorter.rb
@@ -9,12 +9,18 @@ module Codex
 
     def generate(site)
       site.collections['codex'].docs.each do |page|
-        path_arr = Pathname(page.path).each_filename.to_a
-        branch = path_arr[-3]
-        topic = path_arr[-2]
-        page.data['branch'] = branch
-        page.data['topic'] = topic
+        next if page.data['slug'] == 'intro'
+
+        add_data(page)
       end
+    end
+
+    def add_data(page)
+      path_arr = Pathname(page.path).each_filename.to_a
+      branch = path_arr[-3]
+      topic = path_arr[-2]
+      page.data['branch'] = branch
+      page.data['topic'] = topic
     end
   end
 end
